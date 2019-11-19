fc4bc29e58ca19d617f18452cb8d3576a508e269
Julien Blatecky <julien.blatecky@creatiwity.net>
2019-11-19 10:53:30 +0100

    Add message loading

--- src/js/forum.js
+++ src/js/forum.js
@@ -1,21 +1,33 @@
 import ky from "ky";
+import $ from "jquery";
+
+function getMessageView(message) {
+  return `<div class="card my-3">
+      <div class="card-body">
+        <p class="card-text">${message.content}</p>
+      </div>
+      <div class="card-footer text-muted text-right">
+        Par ${message.author}, ${message.timestamp}
+      </div>
+    </div>`;
+}
+
+function displayMessages(messages) {
+  const $messagesContainer = $(".messages-container");
+
+  // Clear list content on view
+  $messagesContainer.empty();
+
+  // Iterate on messages and display getMessageView(message);
+  messages.forEach(message => {
+    $messagesContainer.append(getMessageView(message));
+  });
+}
 
 async function refreshMessages() {
   // GET https://ensmn.herokuapp.com/messages
   const messages = await ky.get("https://ensmn.herokuapp.com/messages").json();
   displayMessages(messages);
-}
-
-function displayMessages(messages) {
-  // Clear list content on view
-  // Iterate on messages and display getMessageView(message);
-}
-
-function getMessageView(message) {
-  return `<div>
-    <p>${message.username}<p/>
-    <p>${message.message}</p>
-  </div>`;
 }
 
 setInterval(() => {
